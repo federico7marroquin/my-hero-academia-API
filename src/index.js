b89c3d24bad56b317ba3252c5a8bb870f0c1da04
federico7marroquin <fmarroquin@be-prosolutions.com>
2021-06-14 22:26:12 -0500

    clase 12 fetch

--- src/index.js
+++ src/index.js
@@ -3,4 +3,26 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+//web api
+//conectarnos al server 
+//procesar la respuesta, y convertirla en JSON 
+// json -> data  -> renderizar info en browser
+// primse -> async/await
+window.fetch(url)
+    .then(response => response.json())
+    .then(responseJson => {
+        const todosLosItems = [];
+        responseJson.data.forEach(item => {
+            const image = document.createElement('img')
+            const title = document.createElement('h2')
+            const price = document.createElement('div')
+            
+            const container = document.createElement('div')
+            container.append(image, title, price)
+            todosLosItems.push(container)    
+        })
+        document.body.append(...todosLosItems)
+    })
+
